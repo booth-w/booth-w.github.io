5f3a23d9d3c30488feeda60a01931cfb61b813f9
Skull_is_dull <booth.w.2005@gmail.com>
2022-06-12 22:55:21 +0100

    Added simple tone

--- music/draw-a-waveform/sketch.js
+++ music/draw-a-waveform/sketch.js
@@ -1,12 +1,27 @@
+let wave;
+let isPlaying = false;
+
 function setup() {
-	createCanvas(200, 200);
-	wave = new p5.Ocillator();
+	let canvas = createCanvas(200, 200);
+	canvas.parent("game");
+	$("#game").append("<div id='winner' style='user-select: none'></div>");
+
+	wave = new p5.Oscillator();
 	wave.setType("sine");
 	wave.amp(1);
-	wave.freq(300);
-	wave.play();	
+	wave.freq(220);
 }
 
 function draw() {
 	background(255);
+}
+
+function mousePressed() {
+	if (isPlaying) {
+		wave.stop();
+		isPlaying = false;
+	} else {
+		wave.start();
+		isPlaying = true;
+	}
 }
