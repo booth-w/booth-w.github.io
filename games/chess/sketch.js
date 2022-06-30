eea84d1c77438ce8c2575da1651ac77b078c6365
Skull_is_dull <booth.w.2005@gmail.com>
2022-07-01 00:33:14 +0100

    Board display

--- games/chess/sketch.js
+++ games/chess/sketch.js
@@ -1,15 +1,44 @@
+let board;
+
 function setup() {
 	createCanvas(400, 400);
+	fill(255);
+	textAlign(CENTER);
+
+	board = new Board();
 }
 
 function draw() {
-	backgrond(100);
+	background(color("#161C2C"));
+	board.display();
+}
+
+class Board {
+	constructor() {
+		this.board = [
+			[new Rook("Black"), new Knight("Black"), new Bishop("Black"), new Queen("Black"), new King("Black"), new Bishop("Black"), new Knight("Black"), new Rook("Black")],
+			[new Pawn("Black"), new Pawn("Black"), new Pawn("Black"), new Pawn("Black"), new Pawn("Black"), new Pawn("Black"), new Pawn("Black"), new Pawn("Black")],
+			[],
+			[],
+			[],
+			[],
+			[new Pawn("White"), new Pawn("White"), new Pawn("White"), new Pawn("White"), new Pawn("White"), new Pawn("White"), new Pawn("White"), new Pawn("White")],
+			[new Rook("White"), new Knight("White"), new Bishop("White"), new Queen("White"), new King("White"), new Bishop("White"), new Knight("White"), new Rook("White")]
+		];
+		this.boardTemp = JSON.parse(JSON.stringify(this.board));
+	}
+
+	display() {
+		for (let row = 0; row < 8; row++) {
+			for (let col = 0; col < 8; col++) {
+				if (this.board[row][col]) text(this.board[row][col].piece, 50 * (col+0.5), 50 * (row+0.5));
+			}
+		}
+	}
 }
 
 class Piece {
-	constructor(x, y, colour) {
-		this.x = x;
-		this.y = y;
+	constructor(colour) {
 		this.colour = colour;
 	}
 
@@ -19,42 +48,67 @@ class Piece {
 }
 
 class Pawn extends Piece {
+	constructor(colour) {
+		super(colour);
+		this.piece = "P";
+	}
+
 	move() {
 
 	}
 }
 
 class Rook extends Piece {
+	constructor(colour) {
+		super(colour);
+		this.piece = "R";
+	}
+
 	move() {
 
 	}
 }
 
 class Bishop extends Piece {
+	constructor(colour) {
+		super(colour);
+		this.piece = "B";
+	}
+
 	move() {
 
 	}
 }
 
 class Knight extends Piece {
+	constructor(colour) {
+		super(colour);
+		this.piece = "K";
+	}
+
 	move() {
 
 	}
 }
 
 class King extends Piece {
+	constructor(colour) {
+		super(colour);
+		this.piece = "K";
+	}
+
 	move() {
 
 	}
 }
 
 class Queen extends Piece {
+	constructor(colour) {
+		super(colour);
+		this.piece = "Q";
+	}
+
 	move() {
 
 	}
-}
-
-class Board {
-	board = [];
-	boardTemp = [];
 }
