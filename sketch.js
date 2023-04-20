73497e9f6b811c11f53d62e5ffaf26aafe8c6c0d
Skull_is_dull <booth.w.2005@gmail.com>
2023-04-20 12:10:01 +0100

    Background

---
+++ sketch.js
@@ -0,0 +1,54 @@
+let points = [];
+
+function setup() {
+	createCanvas(windowWidth, windowHeight / 1);
+	fill(200);
+
+	for (let i = 0; i < 10; i++) {
+		points.push(new Point(random(width), random(height)));
+	}
+}
+
+function draw() {
+	background(100);
+	points.forEach(point => {
+		point.move();
+		point.drawLines();
+	});
+	points.forEach(point => {
+		point.draw();
+	});
+}
+
+class Point {
+	constructor(x, y) {
+		this.pos = createVector(x, y);
+		this.vel = p5.Vector.random2D();
+	}
+
+	move() {
+		this.pos.add(this.vel);
+		this.pos.x = (this.pos.x + width) % width;
+		this.pos.y = (this.pos.y + height) % height;
+	}
+	
+	draw() {
+		stroke(200);
+		ellipse(this.pos.x, this.pos.y, 20, 20);
+	}
+
+	drawLines() {
+		for (let a = 0; a < points.length; a++) {
+			let dist = p5.Vector.dist(this.pos, points[a].pos);
+			stroke(150);
+			strokeWeight(30 - dist / 15);
+			if ((dist / 15 < 30)) {
+				line(this.pos.x, this.pos.y, points[a].pos.x, points[a].pos.y);
+			}
+		}
+	}
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
