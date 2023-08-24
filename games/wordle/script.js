d631203cd9ef2017e72c0e8196f09f7732efd6a9
booth-w <booth.w.2005@gmail.com>
2023-08-24 16:14:45 +0100

    Revert "New word button"
    This reverts commit 15c50d5428ef0742786ed2b4ddb7c181db8be4dc.

--- games/wordle/script.js
+++ games/wordle/script.js
@@ -9,25 +9,19 @@ client.onreadystatechange = () => {
 }
 client.send();
 
-function newWord() {
+// function newWord() {
 	word = words[Math.floor(Math.random() * 1000)];
 	rowCurrent = 0;
-	
-	$("td").each((i, e) => {
-		e.innerHTML = "";
-		e.style.backgroundColor = "var(--bg-low)";
-	});
-	
-	$(".letter").each((i, e) => {
-		e.style.backgroundColor = "var(--bg-low)";
-	});
-
+	// $("#table").html("");
+	for (let a = 0; a < 6; a++) {
+		// $("#table").append("<tr><td class='box'></td><td class='box'></td><td class='box'></td><td class='box'></td><td class='box'></td></tr>");
+	}
 	$("#answer").css("display", "none");
 	$("#textBox").css("display", "inline-block");
 	$("#textBox").focus();
-}
+// }
 
-newWord();
+// newWord();
 
 function toggleHelp() {
 	$(".game").css("filter", "blur(2px)");
@@ -45,13 +39,14 @@ window.onclick = (event) => {
 
 $("#textBox").keypress((e) => {
 	if (e.which == 13) {
-		let text = $("#textBox").val().toLowerCase();
+		let text = $("#textBox").val().toLowerCase()
 		$("#textBox").val("");
 		if (text.length == 5 && words.includes(text)) {
 			$('#table tr').each((index, tr) => {
 				if (index == rowCurrent) {
 					$(tr).find("td").each((index, td) => {
 						td.innerHTML = text[index];
+						// console.log(text[index]);
 						if (text[index] == word[index]) {
 							td.style.backgroundColor = "var(--green)";
 							$(`#${text[index]}`).css("backgroundColor", "var(--green)");
@@ -63,7 +58,7 @@ $("#textBox").keypress((e) => {
 						}
 					});
 					rowCurrent++;
-					if (rowCurrent >= 6 || text == word) {
+					if (rowCurrent >= 6) {
 						$("#textBox").css("display", "none");
 						$("#answer tr td").each((i, e) => {
 							e.innerHTML = word[i];
