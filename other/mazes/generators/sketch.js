779852f946d7dff9e978b3ffb7f4b8a35c126f77
booth-w <personal@booth-w.co.uk>
2025-10-03 14:51:14 +0100

    Add blue current tile

--- other/mazes/generators/sketch.js
+++ other/mazes/generators/sketch.js
@@ -15,10 +15,10 @@ function setup() {
 }
 
 function draw() {
-	mazeCurrent = mazeGen.next();
+	let	mazeCurrent = mazeGen.next();
 	if (!mazeCurrent.done) {
 		background("#D8DEE9");
-		drawMaze(mazeCurrent.value);
+		drawMaze(mazeCurrent.value[0], mazeCurrent.value[1], mazeCurrent.value[2]);
 	}
 }
 
@@ -26,13 +26,16 @@ function clearMaze() {
 	maze = Array(gridSize).fill().map(() => Array(gridSize).fill(false));
 }
 
-function drawMaze(toDraw) {
+function drawMaze(toDraw, currentX, currentY) {
 	toDraw.forEach((row, y) => {
 		row.forEach((cell, x) => {
 			if (cell) {
 				fill("#D8DEE9");
 			} else {
 				fill("#2E3440");
+			}
+			if (x === currentX && y === currentY) {
+				fill("#5E81AC");
 			}
 			rect(x * (400 / gridSize), y * (400 / gridSize), 400 / gridSize, 400 / gridSize);
 		});
@@ -45,7 +48,7 @@ class Generators {
 
 		function* carve(x, y) {
 			maze[y][x] = true;
-			yield maze;
+			yield [maze, x, y];
 
 			for (let [dx, dy] of shuffle(directions)) {
 				let nx = x + dx * 2;
@@ -53,6 +56,7 @@ class Generators {
 
 				if (nx >= 0 && nx < 400/gridSize && ny >= 0 && ny < 400/gridSize && !maze[ny][nx]) {
 					maze[y + dy][x + dx] = true;
+					yield [maze, x + dx, y + dy];
 					yield* carve(nx, ny);
 				}
 			}
